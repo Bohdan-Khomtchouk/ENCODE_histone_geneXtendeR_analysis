3654fa756211ee9617cbaded2a67b23f34a7b193
Bohdan Khomtchouk <bohdankhomtchouk@um-31hfg8wm.attlocal.net>
2017-07-31 19:02:57 -0400

    commit graphs

---
+++ histone_ENCODE/0-10000/distal/H3K27ac/separate.sh
@@ -0,0 +1,45 @@
+grep "A549" ENCODE_DISTAL_H3K27ac.bed > block1.txt
+grep "AG04450" ENCODE_DISTAL_H3K27ac.bed > block2.txt
+grep "CD14 Primary Cells" ENCODE_DISTAL_H3K27ac.bed > block3.txt
+grep "CD19 Primary Cells" ENCODE_DISTAL_H3K27ac.bed > block4.txt
+grep "CD3 Primary Cells" ENCODE_DISTAL_H3K27ac.bed > block5.txt
+grep "CD56 Primary Cells" ENCODE_DISTAL_H3K27ac.bed > block6.txt
+grep "CD8 Primary Cells" ENCODE_DISTAL_H3K27ac.bed > block7.txt
+grep "Fetal Adrenal Gland" ENCODE_DISTAL_H3K27ac.bed > block8.txt
+grep "Fetal Intestine Large" ENCODE_DISTAL_H3K27ac.bed > block9.txt
+grep "Fetal Intestine Small" ENCODE_DISTAL_H3K27ac.bed > block10.txt
+grep "Fetal Muscle Leg" ENCODE_DISTAL_H3K27ac.bed > block11.txt
+grep "Fetal Muscle Trunk" ENCODE_DISTAL_H3K27ac.bed > block12.txt
+grep "Fetal Placenta" ENCODE_DISTAL_H3K27ac.bed > block13.txt
+grep "Fetal Stomach" ENCODE_DISTAL_H3K27ac.bed > block14.txt
+grep "Fetal Thymus" ENCODE_DISTAL_H3K27ac.bed > block15.txt
+grep "Gastric" ENCODE_DISTAL_H3K27ac.bed > block16.txt
+grep "GM12878" ENCODE_DISTAL_H3K27ac.bed > block17.txt
+grep "H1 BMP4 Derived Mesendoderm Cultured Cells" ENCODE_DISTAL_H3K27ac.bed > block18.txt
+grep "H1 BMP4 Derived Trophoblast Cultured Cells" ENCODE_DISTAL_H3K27ac.bed > block19.txt
+grep "H1 Derived Mesenchymal Stem Cells" ENCODE_DISTAL_H3K27ac.bed > block20.txt
+grep "H1 Derived Neuronal Progenitor Cultured Cells" ENCODE_DISTAL_H3K27ac.bed > block21.txt
+grep "H1-hESC" ENCODE_DISTAL_H3K27ac.bed > block22.txt
+grep "HCT-116" ENCODE_DISTAL_H3K27ac.bed > block23.txt
+grep "HeLa-S3" ENCODE_DISTAL_H3K27ac.bed > block24.txt
+grep "HepG2" ENCODE_DISTAL_H3K27ac.bed > block25.txt
+grep "HMEC" ENCODE_DISTAL_H3K27ac.bed > block26.txt
+grep "HSMM" ENCODE_DISTAL_H3K27ac.bed > block27.txt
+grep "HSMMtube" ENCODE_DISTAL_H3K27ac.bed > block28.txt
+grep "HUVEC" ENCODE_DISTAL_H3K27ac.bed > block29.txt
+grep "IMR90" ENCODE_DISTAL_H3K27ac.bed > block30.txt
+grep "iPS DF 19 11" ENCODE_DISTAL_H3K27ac.bed > block31.txt
+grep "K562" ENCODE_DISTAL_H3K27ac.bed > block32.txt
+grep "MCF-7" ENCODE_DISTAL_H3K27ac.bed > block33.txt
+grep "Monocytes-CD14+ RO01746" ENCODE_DISTAL_H3K27ac.bed > block34.txt
+grep "NH-A" ENCODE_DISTAL_H3K27ac.bed > block36.txt
+grep "NHDF-Ad" ENCODE_DISTAL_H3K27ac.bed > block37.txt
+grep "NHEK" ENCODE_DISTAL_H3K27ac.bed > block38.txt
+grep "NHLF" ENCODE_DISTAL_H3K27ac.bed > block39.txt
+grep "Ovary" ENCODE_DISTAL_H3K27ac.bed > block40.txt
+grep "PANC-1" ENCODE_DISTAL_H3K27ac.bed > block41.txt
+grep "Pancreas" ENCODE_DISTAL_H3K27ac.bed > block42.txt
+grep "Penis Foreskin Fibroblast Primary Cells" ENCODE_DISTAL_H3K27ac.bed > block43.txt
+grep "Penis Foreskin Keratinocyte Primary Cells" ENCODE_DISTAL_H3K27ac.bed > block44.txt
+grep "Psoas Muscle" ENCODE_DISTAL_H3K27ac.bed > block45.txt
+grep "Small Intestine" ENCODE_DISTAL_H3K27ac.bed > block46.txt
---
+++ histone_ENCODE/0-10000/distal/H3K4me3/separate.sh
@@ -0,0 +1,94 @@
+grep "A549" ENCODE_DISTAL_H3K4me3.Bed > block1.txt
+grep "AG04449" ENCODE_DISTAL_H3K4me3.Bed > block2.txt
+grep "AG04450" ENCODE_DISTAL_H3K4me3.Bed > block3.txt
+grep "AG09309" ENCODE_DISTAL_H3K4me3.Bed > block4.txt
+grep "AG09319" ENCODE_DISTAL_H3K4me3.Bed > block5.txt
+grep "AG10803" ENCODE_DISTAL_H3K4me3.Bed > block6.txt
+grep "AoAF" ENCODE_DISTAL_H3K4me3.Bed > block7.txt
+grep "BE2 C" ENCODE_DISTAL_H3K4me3.Bed > block8.txt
+grep "BJ" ENCODE_DISTAL_H3K4me3.Bed > block9.txt
+grep "Breast vHMEC" ENCODE_DISTAL_H3K4me3.Bed > block10.txt
+grep "Caco-2" ENCODE_DISTAL_H3K4me3.Bed > block11.txt
+grep "CD14 Primary Cells" ENCODE_DISTAL_H3K4me3.Bed > block12.txt
+grep "CD19 Primary Cells" ENCODE_DISTAL_H3K4me3.Bed > block13.txt
+grep "CD20+ RO01778" ENCODE_DISTAL_H3K4me3.Bed > block14.txt
+grep "CD3 Primary Cells" ENCODE_DISTAL_H3K4me3.Bed > block15.txt
+grep "CD4 Primary Cells" ENCODE_DISTAL_H3K4me3.Bed > block16.txt
+grep "CD56 Primary Cells" ENCODE_DISTAL_H3K4me3.Bed > block17.txt
+grep "CD8 Primary Cells" ENCODE_DISTAL_H3K4me3.Bed > block18.txt
+grep "Fetal Adrenal Gland" ENCODE_DISTAL_H3K4me3.Bed > block19.txt
+grep "Fetal Brain" ENCODE_DISTAL_H3K4me3.Bed > block20.txt
+grep "Fetal Heart" ENCODE_DISTAL_H3K4me3.Bed > block21.txt
+grep "Fetal Intestine Large" ENCODE_DISTAL_H3K4me3.Bed > block22.txt
+grep "Fetal Intestine Small" ENCODE_DISTAL_H3K4me3.Bed > block23.txt
+grep "Fetal Kidney" ENCODE_DISTAL_H3K4me3.Bed > block24.txt
+grep "Fetal Lung" ENCODE_DISTAL_H3K4me3.Bed > block25.txt
+grep "Fetal Muscle Leg" ENCODE_DISTAL_H3K4me3.Bed > block26.txt
+grep "Fetal Muscle Trunk" ENCODE_DISTAL_H3K4me3.Bed > block27.txt
+grep "Fetal Placenta" ENCODE_DISTAL_H3K4me3.Bed > block28.txt
+grep "Fetal Stomach" ENCODE_DISTAL_H3K4me3.Bed > block29.txt
+grep "Fetal Thymus" ENCODE_DISTAL_H3K4me3.Bed > block30.txt
+grep "Gastric" ENCODE_DISTAL_H3K4me3.Bed > block31.txt
+grep "GM06990" ENCODE_DISTAL_H3K4me3.Bed > block32.txt
+grep "GM12864" ENCODE_DISTAL_H3K4me3.Bed > block33.txt
+grep "GM12865" ENCODE_DISTAL_H3K4me3.Bed > block34.txt
+grep "GM12878" ENCODE_DISTAL_H3K4me3.Bed > block35.txt
+grep "H1 BMP4 Derived Mesendoderm Cultured Cells" ENCODE_DISTAL_H3K4me3.Bed > block36.txt
+grep "H1 BMP4 Derived Trophoblast Cultured Cells" ENCODE_DISTAL_H3K4me3.Bed > block37.txt
+grep "H1 Derived Mesenchymal Stem Cells" ENCODE_DISTAL_H3K4me3.Bed > block38.txt
+grep "H1 Derived Neuronal Progenitor Cultured Cells" ENCODE_DISTAL_H3K4me3.Bed > block39.txt
+grep "H1-hESC" ENCODE_DISTAL_H3K4me3.Bed > block40.txt
+grep "H7-hESC" ENCODE_DISTAL_H3K4me3.Bed > block41.txt
+grep "HAc" ENCODE_DISTAL_H3K4me3.Bed > block42.txt
+grep "HA-sp" ENCODE_DISTAL_H3K4me3.Bed > block43.txt
+grep "HBMEC" ENCODE_DISTAL_H3K4me3.Bed > block44.txt
+grep "HCF" ENCODE_DISTAL_H3K4me3.Bed > block45.txt
+grep "HCFaa" ENCODE_DISTAL_H3K4me3.Bed > block46.txt
+grep "HCM" ENCODE_DISTAL_H3K4me3.Bed > block47.txt
+grep "HCPEpiC" ENCODE_DISTAL_H3K4me3.Bed > block48.txt
+grep "HCT-116" ENCODE_DISTAL_H3K4me3.Bed > block49.txt
+grep "Heart" ENCODE_DISTAL_H3K4me3.Bed > block50.txt
+grep "HEEpiC" ENCODE_DISTAL_H3K4me3.Bed > block51.txt
+grep "HeLa-S3" ENCODE_DISTAL_H3K4me3.Bed > block52.txt
+grep "HepG2" ENCODE_DISTAL_H3K4me3.Bed > block53.txt
+grep "HFF" ENCODE_DISTAL_H3K4me3.Bed > block54.txt
+grep "HFF-Myc" ENCODE_DISTAL_H3K4me3.Bed > block55.txt
+grep "HL-60" ENCODE_DISTAL_H3K4me3.Bed > block56.txt
+grep "HMEC" ENCODE_DISTAL_H3K4me3.Bed > block57.txt
+grep "HMF" ENCODE_DISTAL_H3K4me3.Bed > block58.txt
+grep "HPAF" ENCODE_DISTAL_H3K4me3.Bed > block59.txt
+grep "HPF" ENCODE_DISTAL_H3K4me3.Bed > block60.txt
+grep "HRE" ENCODE_DISTAL_H3K4me3.Bed > block61.txt
+grep "HRPEpiC" ENCODE_DISTAL_H3K4me3.Bed > block62.txt
+grep "HSMM" ENCODE_DISTAL_H3K4me3.Bed > block63.txt
+grep "HSMMtube" ENCODE_DISTAL_H3K4me3.Bed > block64.txt
+grep "HUVEC" ENCODE_DISTAL_H3K4me3.Bed > block65.txt
+grep "HVMF" ENCODE_DISTAL_H3K4me3.Bed > block66.txt
+grep "IMR90" ENCODE_DISTAL_H3K4me3.Bed > block67.txt
+grep "iPS DF 19 11" ENCODE_DISTAL_H3K4me3.Bed > block68.txt
+grep "Jurkat" ENCODE_DISTAL_H3K4me3.Bed > block69.txt
+grep "K562" ENCODE_DISTAL_H3K4me3.Bed > block70.txt
+grep "LNCaP" ENCODE_DISTAL_H3K4me3.Bed > block71.txt
+grep "MCF-7" ENCODE_DISTAL_H3K4me3.Bed > block72.txt
+grep "Monocytes-CD14+ RO01746" ENCODE_DISTAL_H3K4me3.Bed > block73.txt
+grep "NB4" ENCODE_DISTAL_H3K4me3.Bed > block75.txt
+grep "NH-A" ENCODE_DISTAL_H3K4me3.Bed > block76.txt
+grep "NHDF-Ad" ENCODE_DISTAL_H3K4me3.Bed > block77.txt
+grep "NHDF-neo" ENCODE_DISTAL_H3K4me3.Bed > block78.txt
+grep "NHEK" ENCODE_DISTAL_H3K4me3.Bed > block79.txt
+grep "NHLF" ENCODE_DISTAL_H3K4me3.Bed > block80.txt
+grep "NT2-D1" ENCODE_DISTAL_H3K4me3.Bed > block81.txt
+grep "Ovary" ENCODE_DISTAL_H3K4me3.Bed > block82.txt
+grep "PANC-1" ENCODE_DISTAL_H3K4me3.Bed > block83.txt
+grep "Pancreas" ENCODE_DISTAL_H3K4me3.Bed > block84.txt
+grep "Penis Foreskin Fibroblast Primary Cells" ENCODE_DISTAL_H3K4me3.Bed > block85.txt
+grep "Penis Foreskin Keratinocyte Primary Cells" ENCODE_DISTAL_H3K4me3.Bed > block86.txt
+grep "Psoas Muscle" ENCODE_DISTAL_H3K4me3.Bed > block87.txt
+grep "RPTEC" ENCODE_DISTAL_H3K4me3.Bed > block88.txt
+grep "SAEC" ENCODE_DISTAL_H3K4me3.Bed > block89.txt
+grep "SKMC" ENCODE_DISTAL_H3K4me3.Bed > block90.txt
+grep "SK-N-MC" ENCODE_DISTAL_H3K4me3.Bed > block91.txt
+grep "SK-N-SH RA" ENCODE_DISTAL_H3K4me3.Bed > block92.txt
+grep "Small Intestine" ENCODE_DISTAL_H3K4me3.Bed > block93.txt
+grep "WERI-Rb-1" ENCODE_DISTAL_H3K4me3.Bed > block94.txt
+grep "WI-38" ENCODE_DISTAL_H3K4me3.Bed > block95.txt
